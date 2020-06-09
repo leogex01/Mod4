7afb3dd98038c7fc84e61a66f70eef176ff17048
leogex01 <jgelardi@zoho.com>
2020-06-09 17:04:45 -0500

    Added 'places  = 2' value

--- test/test_coupon-calculations.py
+++ test/test_coupon-calculations.py
@@ -1,14 +1,14 @@
 import unittest
-from store import  coupon_calculations
+from store import coupon_calculations
 
 class MyTestCase(unittest.TestCase):
     def test_price_under_ten(self):
-        self.assertAlmostEqual(5.95,(coupon_calculations(9, 5, 10)))
-        self.assertAlmostEqual(5.95,(coupon_calculations(4, 5, 15)))
-        self.assertAlmostEqual(5.95,(coupon_calculations(9.95, 5, 20)))
-        self.assertAlmostEqual((5.95,(coupon_calculations(7.13, 10,10))))
-        self.assertAlmostEqual((5.95,(coupon_calculations(-1, 10, 15))))
-        self.assertAlmostEqual((5.95,(coupon_calculations(0, 10, 20))))
+        self.assertAlmostEqual(coupon_calculations(9, 5, 10),5.95, places=2)
+        self.assertAlmostEqual(coupon_calculations(4, 5, 15), 5.95,places=2)
+        self.assertAlmostEqual(coupon_calculations(9.95, 5, 20),5.95,places=2)
+        self.assertAlmostEqual(coupon_calculations(7.13, 10, 10),5.95,places=2)
+        self.assertAlmostEqual(coupon_calculations(-1, 10, 15),5.95,places=2)
+        self.assertAlmostEqual(coupon_calculations(0, 10, 20),5.95,places=2)
 
 
 
