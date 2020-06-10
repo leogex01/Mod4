0ff6fdeb699c202f61a941472c7d1eca8bfdec34
leogex01 <jgelardi@zoho.com>
2020-06-10 15:41:18 -0500

    Added negative in second spot

--- Main/test/test_validation_with_try.py
+++ Main/test/test_validation_with_try.py
@@ -1,3 +1,10 @@
+"""
+Program :test_validation_with_try.py
+Author : Chris Geralds
+Date : 06/10/202
+A test to check a raised value error on negatives for averaged test scores
+"""
+
 import unittest
 from Main.input_validation import validation_with_try as vw
 
@@ -5,6 +12,7 @@ class MyTestCase(unittest.TestCase):
     def test_average_negative_exception(self):
         with self.assertRaises(ValueError):
             vw.average(-90, 89, 78)
+            #vw.average(90, -9, 45)
 
 
 
