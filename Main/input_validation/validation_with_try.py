97a34e4ff92970ae0d69f26c7d72197df74ecebe
leogex01 <jgelardi@zoho.com>
2020-06-10 16:48:01 -0500

    3rd score negative

--- Main/input_validation/validation_with_try.py
+++ Main/input_validation/validation_with_try.py
@@ -9,7 +9,7 @@ import unittest
 
 def average(score1, score2, score3):
     NUMBER_TESTS = 3
-    if score1 < 0 or score2 < 0:
+    if score1 < 0 or score2 < 0 or score3 < 0:
         raise ValueError("Only positive integers are allowed")
     return float((score1 + score2 + score3) / NUMBER_TESTS)
 
