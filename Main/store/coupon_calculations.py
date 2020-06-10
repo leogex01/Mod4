ed58acc31fdc1ebf5977114b619e79f239c91122
leogex01 <jgelardi@zoho.com>
2020-06-10 05:05:47 -0500

    Tidied up code
    Found a typo in 10 or less section, never put in '=' sign.

--- Main/store/coupon_calculations.py
+++ Main/store/coupon_calculations.py
@@ -9,30 +9,23 @@ Program to total a bill plus tax and shipping using inputted price, cash-discoun
 def calculate_order(pr, cashdis, perdiscount):
     taxrate = .06
     cash_subtotal = float(pr) - float(cashdis)
-    print(cash_subtotal)
-    subtotal = cash_subtotal - (cash_subtotal * float(perdiscount) / 100)
+    if perdiscount == 0:
+        subtotal = cash_subtotal
+    else:
+        subtotal = cash_subtotal - (cash_subtotal * perdiscount/100)
     tax = subtotal * taxrate
-    print(subtotal)
-    if subtotal < 10:
+    if subtotal <= 10:
         shipping = 5.95
-        total = subtotal + shipping + tax
-        print(f'shipping:{shipping} \n' f'Sales tax: {tax} ')
-        return total
+        return round(subtotal + shipping + tax, 2)
     elif 10 < subtotal < 30:
         shipping = 7.95
-        total = subtotal + shipping + tax
-        print(f'shipping:{shipping} \n' f'Sales tax: {tax} ')
-        return total
+        return round(subtotal + shipping + tax, 2)
     elif 30 < subtotal < 50:
         shipping = 11.95
-        total = subtotal + shipping + tax
-        print(f'shipping:{shipping} \n' f'Sales tax: {tax} ')
-        return total
+        return round(subtotal + shipping + tax, 2)
     elif subtotal >= 50:
         shipping = 0
-        total = subtotal + shipping + tax
-        print(f'shipping:{shipping} \n' f'Sales tax: {tax} ')
-        return total
+        return round(subtotal + shipping + tax, 2)
 
 
 
@@ -40,7 +33,7 @@ def calculate_order(pr, cashdis, perdiscount):
 
 if __name__ == '__main__':
     price = float(input('Please enter price: '))
-    cash = float(input('Enter cash off coupon amount; '))
+    cash = float(input('Enter cash off coupon amount: '))
     percent = float(input('Enter percentage discount amount: '))
     calculate_order(price, cash, percent)
     print(f'Your total is {calculate_order(price, cash, percent)}')
