346028522562e053d0b83c57ac651f4420a3e5f8
leogex01 <jgelardi@zoho.com>
2020-06-10 16:45:08 -0500

    1st score is negative

--- Main/input_validation/validation_with_try.py
+++ Main/input_validation/validation_with_try.py
@@ -4,13 +4,22 @@ Author : Chris Geralds
 Date : 06/10/2020
 Small program to take three test scores and average them out
 """
+import unittest
+
+
 def average(score1, score2, score3):
-        NUMBER_TESTS = 3
-        return float((score1 + score2 + score3)/NUMBER_TESTS)
-raise ValueError("Only positive integers are allowed")
+    NUMBER_TESTS = 3
+    if score1 < 0 :
+        raise ValueError("Only positive integers are allowed")
+    return float((score1 + score2 + score3) / NUMBER_TESTS)
+
 
 if __name__ == '__main__':
-    __unittest__
-""""
-
-"""
+    last_name = input("Please enter your last name. ")
+    first_name = input("Please enter your first name. ")
+    age = str(input("Please enter your age. "))
+    test1 = int(input("Enter first score:  "))
+    test2 = int(input("Enter second score:  "))
+    test3 = int(input("Enter last score:  "))
+    average = average(test1, test2, test3)
+    print(f'{last_name}, {first_name} age:{age} average score:  {average}')
