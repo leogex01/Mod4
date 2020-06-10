682a69f0b567b03fcbfec807e4e70c784875a347
leogex01 <jgelardi@zoho.com>
2020-06-10 15:46:38 -0500

    Value Error

---
+++ Main/input_validation/validation_with_try.py
@@ -0,0 +1,16 @@
+"""
+Program : validation_with_try.py
+Author : Chris Geralds
+Date : 06/10/2020
+Small program to take three test scores and average them out
+"""
+def average(score1, score2, score3):
+        NUMBER_TESTS = 3
+        return float((score1 + score2 + score3)/NUMBER_TESTS)
+raise ValueError("Only positive integers are allowed")
+
+if __name__ == '__main__':
+    __unittest__
+""""
+
+"""
